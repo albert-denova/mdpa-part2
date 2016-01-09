890a5123ea416ba22a7895923e011ef073ce833f
Albert De Nova <albertdn@salleurl.edu>
2016-01-09 09:48:51 +0100

    Make AAR project work

--- PropertyCrossAAR/app/src/main/java/com/lasalle/second/part/propertycrossaar/MainActivity.java
+++ PropertyCrossAAR/app/src/main/java/com/lasalle/second/part/propertycrossaar/MainActivity.java
@@ -3,5 +3,5 @@ package com.lasalle.second.part.propertycrossaar;
 /**
  * Created by albert.denova on 22/12/15.
  */
-public class MainActivity extends com.lasalle.second.part.week1.MainActivity  {
+public class MainActivity extends com.lasalle.second.part.week1.activities.MainActivity  {
 }
